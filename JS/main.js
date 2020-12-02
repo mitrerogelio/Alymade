21a6fea62060436a13591665b038cfd1aaf6a3dd
Rogelio Mitre <rogelinmitre@gmail.com>
2020-12-01 18:18:39 -0600

    create gallery page

---
+++ JS/main.js
@@ -0,0 +1,6 @@
+const feed = new Instafeed({
+    accessToken: 'IGQVJWd2d3VHNOcjNzSmxmcFhIRGJzdjNMWE9iWEZAYajhLZATJtb0hYaTJLVnNGdEtNSldYYTZAYdHB1czlsMzZAXeHVaSnBTekRqd3FNbUVMQ3ktNnVqYUMwSV9JS1dMZAnZARQVhqczF5d3AwZAmZAzRGlXMwZDZD'
+  });
+  feed.run();
+
+  console.log('yeeeet');
